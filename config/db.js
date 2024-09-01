e21a2741feb62f6a42da972f3548dc2c2d78bd63
CodeAnt AI <chinmay@codeant.ai>
2024-09-01 09:20:27 +0000

    changed files

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDBFunction = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
       //   serverApi: ServerApiVersion.v1,
     });
     console.log(`MongoDB connected ${conn.connection.host}`);
