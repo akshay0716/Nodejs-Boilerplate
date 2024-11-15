8c8343cce5b9cbbcf8bcdc7805f974ae3f03254a
akshay nagargoje <akshay.nagargoje@myrl.tech>
2024-11-15 12:47:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 5000;
 
+console.log("hello")
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
